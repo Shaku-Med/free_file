9c7757973c947103f8d51374d87673d123861c12
Shaku-Med <jujubelt124@gmail.com>
2025-10-27 04:35:07 -0400

    l

--- app/app/routes.ts
+++ app/app/routes.ts
@@ -6,7 +6,7 @@ export default [
     route(`api`, 'routes/Api/layout.tsx', [
         route(`upload`, 'routes/Api/upload/index.tsx'),
         route(`load/video/*`, 'routes/Api/load/Video/index.tsx'),
-        route(`load/image/*`, 'routes/Api/load/Image/index.tsx'),
+        route(`load/image/*`, 'routes/Api/load/image/index.tsx'),
         route(`get/*`, 'routes/Api/get/index.tsx'),
     ]),
     route(`:id`, 'routes/Dynamic/layout.tsx', [
