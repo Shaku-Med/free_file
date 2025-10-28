1b60c3618a23ee0eee9aacaab84801fd0ca59982
Shaku-Med <jujubelt124@gmail.com>
2025-10-27 22:59:52 -0400

    Finished and pushed

--- app/app/lib/utils.ts
+++ app/app/lib/utils.ts
@@ -11,5 +11,6 @@ export const arrangeDateForThumbnail = (created_at: string) => {
   const day = date.getUTCDate().toString().padStart(2, '0')
   const month = (date.getUTCMonth() + 1).toString().padStart(2, '0')
   const year = date.getUTCFullYear()
+  console.log('day: ', day, 'month: ', month, 'year: ', year)
   return `${day}_${month}_${year}`
 }
