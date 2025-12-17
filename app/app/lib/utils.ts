1956f5a695ef2b5cc85737e0b8bbc764365e1477
Shaku-Med <jujubelt124@gmail.com>
2025-12-16 19:56:35 -0500

    Remove client bug

--- app/app/lib/utils.ts
+++ app/app/lib/utils.ts
@@ -1,4 +1,3 @@
-import { pipeline, type ImageClassificationSingle } from "@huggingface/transformers"
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 
@@ -6,14 +5,12 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
-
 export const arrangeDateForThumbnail = (created_at: string, retryAttempt: number = 0) => {
   const date = new Date(created_at)
   const dayNumber = retryAttempt >= 1 ? date.getUTCDate() - 1 : date.getUTCDate()
   const day = dayNumber.toString().padStart(2, '0')
   const month = (date.getUTCMonth() + 1).toString().padStart(2, '0')
   const year = date.getUTCFullYear()
-  // console.log('day: ', day, 'month: ', month, 'year: ', year)
   return `${day}_${month}_${year}`
 }
 
@@ -32,13 +29,4 @@ export const getRandomThumbnail = (thumbnails?: string[]): string | null => {
   }
   const randomIndex = Math.floor(Math.random() * thumbnails.length)
   return thumbnails[randomIndex]
-}
-
-
-
-export const CheckNSFW = async (imageUrl: string): Promise<boolean> => {
-  let pip = await pipeline('image-classification', 'AdamCodd/vit-base-nsfw-detector');
-  let result = await pip(imageUrl);
-  let isNSFW = (result as ImageClassificationSingle[])[0].label === 'nsfw'
-  return isNSFW
 }
